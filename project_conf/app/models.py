c8a10f72fdd3de0964e3b0d934741597b8161861
IanTth <ianrendrick@gmail.com>
2022-12-02 22:01:23 -0300

    classes

--- project_conf/app/models.py
+++ project_conf/app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Doctor(models.Model):
+    nome = models.CharField(max_length=50)
+    espec = models.CharField(max_length=50)
+    cpf = models.IntegerField()
+    crm = models.IntegerField()
+    tel = models.IntegerField()
+
+class Paciente(models.Model):
+    nome = models.CharField(max_length=50)
+    endereço = models.TextField
+    genero = models.CharField()
+    cpf = models.IntegerField()
+    tel = models.IntegerField(null=True)
+
+
+class Consulta(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
+    data = models.DateField()
+    hora = models.TimeField()
