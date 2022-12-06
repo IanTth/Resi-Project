8b7f01d3994820cb3666d524d502fdd37b983b08
IanTth <ianrendrick@gmail.com>
2022-12-05 23:17:15 -0300

    Co-authored-by: Maria Fernanda <fernandaguedess@users.noreply.github.com> Co-authored-by: Rafael  lobo <RafaelLobox@users.noreply.github.com>

--- project_conf/app/urls.py
+++ project_conf/app/urls.py
@@ -5,7 +5,11 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    url(r'home', views.home, name='home'),
-    url(r'', views.login, name='login' ),
+    url(r'admin_login/', views.login_admin, name='login_admin'),
+    url(r'logout/', views.logout_admin, name='logout_admin'),
+    url(r'paciente/', views.paciente, name='paciente'),
+    url(r'consulta/', views.consulta, name='consulta'),
+    url(r'doctor/', views.medico, name='doctor'),
+    url(r'', views.home, name='home'),
 
 ] + static(settings.STATIC_URL)
--- project_conf/app/views.py
+++ project_conf/app/views.py
@@ -1,14 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login , logout
 
-def login(request):
 
-    return render(request,'login.html')
-    
 def home(request):
-
+    if not request.user.is_staff:
+        return redirect('login')
     return render(request,'home.html')
 
+def login_admin(request):
+    error = ""
+    if request.method == 'POST':
+        login = request.POST['login']
+        senha = request.POST['senha']
+        user = authenticate(username=login, password = senha)
+        try:
+            if user.is_staff:
+                login(request,user)
+                error = "N"
+            else:
+                error= "Y"
+        except:
+            error = "Y"
+    d = {'error':error}
+    return render(request,'login_admin.html', d)
+    
+def logout_admin(request):
+    if not request.user.is_staff:
+        return redirect('login')
 
 
 
+
+
+def paciente(request): 
+    return render(request,'paciente.html')
+
+def medico(request): 
+    return render(request,'doctor.html')
+
+def consulta(request): 
+    return render(request,'consulta.html')
 
